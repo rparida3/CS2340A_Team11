60a8f1ab62bf8aa743a1ec9adedcb8cbd4f96a2d
sli861 <sli861@gatech.edu>
2023-09-19 15:04:09 -0400

    Added Difficulty Buttons

--- app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
+++ app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
@@ -3,6 +3,7 @@ package com.example.cs2340a_team11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Initial_Config_Screen extends AppCompatActivity {
@@ -17,5 +18,11 @@ public class Initial_Config_Screen extends AppCompatActivity {
         nameBox = (TextView) findViewById(R.id.nameBox);
 
         nameBox.setText("What is your name?");
+
+
+        Button easy = new Button(getApplicationContext());
+        Button medium = new Button(getApplicationContext());
+        Button hard = new Button(getApplicationContext());
+
     }
 }
