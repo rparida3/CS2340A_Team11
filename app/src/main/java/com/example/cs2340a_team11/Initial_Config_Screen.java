d7610d1b092c310c524da1e7b5b9240a3bb5ceb7
admin <dcDvYn5kPCzj3wx4@proton.me>
2023-09-19 14:27:54 -0400

    Created 'initial_config_screen' activity.

---
+++ app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
@@ -0,0 +1,14 @@
+package com.example.cs2340a_team11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Initial_Config_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_config_screen);
+    }
+}
--- app/src/main/java/com/example/cs2340a_team11/MainActivity.java
+++ app/src/main/java/com/example/cs2340a_team11/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.cs2340a_team11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView nameBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameBox = (TextView) findViewById(R.id.nameBox);
+
+        nameBox.setText("What is your name?");
     }
 }
