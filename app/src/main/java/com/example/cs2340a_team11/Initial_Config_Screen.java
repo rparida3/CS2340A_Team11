88b3cf26c595c121a5c2efbc8ceab8164387315a
admin <dcDvYn5kPCzj3wx4@proton.me>
2023-09-19 15:10:19 -0400

    Added user input

--- app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
+++ app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
@@ -5,17 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Initial_Config_Screen extends AppCompatActivity {
-
-    TextView nameBox;
+public class Initial_Config_Screen extends AppCompatActivity { ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_config_screen);
-
-        nameBox = (TextView) findViewById(R.id.nameBox);
-
-        nameBox.setText("What is your name?");
     }
 }
