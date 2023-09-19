0ca2b0c721dee6b24cc88a95724813c5655f1a56
admin <dcDvYn5kPCzj3wx4@proton.me>
2023-09-19 14:35:14 -0400

    Added User Prompt text.

--- app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
+++ app/src/main/java/com/example/cs2340a_team11/Initial_Config_Screen.java
@@ -3,12 +3,19 @@ package com.example.cs2340a_team11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Initial_Config_Screen extends AppCompatActivity {
+
+    TextView nameBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_config_screen);
+
+        nameBox = (TextView) findViewById(R.id.nameBox);
+
+        nameBox.setText("What is your name?");
     }
 }
--- app/src/main/java/com/example/cs2340a_team11/MainActivity.java
+++ app/src/main/java/com/example/cs2340a_team11/MainActivity.java
@@ -7,15 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView nameBox;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        nameBox = (TextView) findViewById(R.id.nameBox);
-
-        nameBox.setText("What is your name?");
     }
 }
