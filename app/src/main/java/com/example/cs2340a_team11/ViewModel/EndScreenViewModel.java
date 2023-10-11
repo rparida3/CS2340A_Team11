4a3cb9f7d530a07fed8cf76365b2b90d8d7f54e9
Yash Prabhu <45132718+yashpr27@users.noreply.github.com>
2023-10-11 11:54:13 -0400

    Add leaderboard to endScreen

--- app/src/main/java/com/example/cs2340a_team11/ViewModel/EndScreenViewModel.java
+++ app/src/main/java/com/example/cs2340a_team11/ViewModel/EndScreenViewModel.java
@@ -8,7 +8,9 @@ import com.example.cs2340a_team11.View.GameScreenActivity;
 public class EndScreenViewModel extends ViewModel {
     private Player player = Player.getPlayer();
     public EndScreenViewModel() {
-
+        Leaderboard leaderboard = Leaderboard.getInstance();
+        leaderboard.addScore(player.getName(), calcTotalScore());
+        ArrayList topScores = leaderboard.getTopScores(5);
     }
 
     // DO PLEASE: fetch results from player and return to view
