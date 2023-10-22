156df57d3c675b11bc7b715cb285c90272ca44b5
Yash Prabhu <45132718+yashpr27@users.noreply.github.com>
2023-10-22 18:40:45 -0400

    Update MovementTests.java

--- app/src/test/java/com/example/cs2340a_team11/MovementTests.java
+++ app/src/test/java/com/example/cs2340a_team11/MovementTests.java
@@ -1,2 +1,37 @@
-package com.example.cs2340a_team11;public class MovementTests {
+package com.example.cs2340a_team11;
+
+import static org.junit.Assert.assertEquals;
+import com.example.cs2340a_team11.Model.Player;
+import org.junit.Test;
+
+public class MovementTests {
+    @Test
+    public void testMoveLeft() {
+        Player.getPlayer().setX(300);
+        Player.getPlayer().setY(500);
+        assertEquals(Player.getPlayer().getX(), 500, 0);
+        assertEquals(Player.getPlayer().getY(), 500, 0);
+
+        Player.getPlayer().moveLeft();
+
+        assertEquals(Player.getPlayer().getX(), 340, 0); // 340 = 500 - 160
+        assertEquals(Player.getPlayer().getY(), 500, 0); // y-coord stays the same
+
+    }
+
+
+    @Test
+    public void testMoveRight() {
+        Player.getPlayer().setX(300);
+        Player.getPlayer().setY(500);
+        assertEquals(Player.getPlayer().getX(), 300, 0);
+        assertEquals(Player.getPlayer().getY(), 500, 0);
+
+        Player.getPlayer().moveRight();
+
+        assertEquals(Player.getPlayer().getX(), 460, 0); // 460 = 300 + 160
+        assertEquals(Player.getPlayer().getY(), 500, 0); // y-coord stays the same
+    }
+
+
 }
