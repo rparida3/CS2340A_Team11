3092c8e9738e3422c0a022793a0527817f4b3fd6
rparida3 <rparida3@gatech.edu>
2023-10-22 20:05:06 -0400

    Added Tests determining if character placement was correct upon entering Map 1

--- app/src/test/java/com/example/cs2340a_team11/Sprint3_JunitTests.java
+++ app/src/test/java/com/example/cs2340a_team11/Sprint3_JunitTests.java
@@ -13,24 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 public class Sprint3_JunitTests {
-    /**
-     * @author Raj Parida
-     * Tests level starting placement between stages 1 & 2.
-     */
-    @Test
-    public void testPlacement1X() {
-        GameScreenViewModel testModel = new GameScreenViewModel();
-        testModel.setCurrMap(1);
-        testModel.setPlayerStarting(1);
-        assertEquals(Player.getPlayer().getX(),3 * BitmapInterface.TILE_SIZE + 320, 0);
-    }
-    @Test
-    public void testPlacement1Y() {
-        GameScreenViewModel testModel = new GameScreenViewModel();
-        testModel.setCurrMap(1);
-        testModel.setPlayerStarting(1);
-        assertEquals(Player.getPlayer().getY(),3 * BitmapInterface.TILE_SIZE, 0);
-    }
+
 
 
 
