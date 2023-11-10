3231969a585d6f8cb4f6780979e6326f2eae29ac
yashpr27 <yashpr27@gmail.com>
2023-11-10 18:27:23 -0500

    Start Enemy Implementations

--- app/src/main/java/com/example/cs2340a_team11/Model/EvilWizard.java
+++ app/src/main/java/com/example/cs2340a_team11/Model/EvilWizard.java
@@ -2,4 +2,20 @@ package com.example.cs2340a_team11.Model;
 
 public class EvilWizard extends Enemy {
 
+    private int stepSize;
+    private int[] colors;
+
+    public EvilWizard(int stepSize) {
+        this.stepSize = stepSize;
+        this.colors(new int[255, 255, 255]);
+    }
+
+    public void setNewStepSize() {
+        this.stepSize = (int) (Math.random() * 3) + 1;
+    }
+
+    public void changeColor() {
+        ;
+    }
+
 }
--- app/src/main/java/com/example/cs2340a_team11/Model/Nightborneidle.java
+++ app/src/main/java/com/example/cs2340a_team11/Model/Nightborneidle.java
@@ -1,5 +1,17 @@
 package com.example.cs2340a_team11.Model;
 
 public class Nightborneidle extends Enemy {
+    private boolean isVisible;
 
+    public Nightborneidle(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
+    }
 }
