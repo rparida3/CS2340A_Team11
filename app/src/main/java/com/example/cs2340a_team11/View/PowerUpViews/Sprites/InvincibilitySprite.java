f5cf1d9c4cae65ffc77ea9fe23af1ab4204742c9
rparida3 <rparida3@gatech.edu>
2023-11-28 15:07:16 -0500

    Added Invincibility Sprite

---
+++ app/src/main/java/com/example/cs2340a_team11/View/PowerUpViews/Sprites/InvincibilitySprite.java
@@ -0,0 +1,12 @@
+package com.example.cs2340a_team11.View.PowerUpViews.Sprites;
+
+import android.content.res.Resources;
+
+import com.example.cs2340a_team11.R;
+
+
+public class InvincibilitySprite extends PowerUpSprite {
+    public InvincibilitySprite(Resources resource) {
+        super(resource, R.drawable.invincibility);
+    }
+}
---
+++ app/src/main/java/com/example/cs2340a_team11/View/PowerUpViews/Views/InvincibilityView.java
@@ -0,0 +1,43 @@
+package com.example.cs2340a_team11.View.PowerUpViews.Views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+import com.example.cs2340a_team11.Model.PowerUpModels.HealthIncrease;
+import com.example.cs2340a_team11.Model.PowerUpModels.Invincibility;
+import com.example.cs2340a_team11.View.PowerUpViews.Sprites.HealthSprite;
+import com.example.cs2340a_team11.View.PowerUpViews.Sprites.InvincibilitySprite;
+
+public class InvincibilityView extends View {
+    private float x;
+    private float y;
+    private InvincibilitySprite invincibilitySprite;
+    private Invincibility invincibility;
+    private int move = 0;
+
+    public InvincibilityView(Context context, float x, float y, Invincibility invincibility) {
+        super(context);
+        this.x = x;
+        this.y = y;
+        this.invincibility = invincibility;
+        invincibilitySprite = new InvincibilitySprite(getResources());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(invincibilitySprite.getSprite(), x, y, null);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+    public Invincibility getHeart() {
+        return this.invincibility;
+    }
+}
